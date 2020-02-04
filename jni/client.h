5fb4895c393ba77d15e34ba9cdc1cb7470904f4e
j4nn <531585+j4nn@users.noreply.github.com>
2020-02-04 22:54:29 +0100

    replace remote calls with primitives from su98

--- jni/client.h
+++ jni/client.h
@@ -4,8 +4,4 @@
 int client_arbitrary_read(uint64_t addr, int size, void *buffer);
 int client_arbitrary_write(uint64_t addr, int size, void *buffer);
 
-int client_curr_get_task_struct_p(uint64_t *addr);
-int client_curr_get_kaslr(uint64_t *addr);
-void client_report_uid(int uid);
-
 #endif
