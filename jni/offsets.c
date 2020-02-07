4f6befea0402fa3b0adad78c1feccb58c252fc26
j4nn <531585+j4nn@users.noreply.github.com>
2020-02-07 01:04:29 +0100

    disable most likely not working targets
    tried to test G8441_47.1.A.2.324, it did not work,
    would most likely need some changes in structure offsets,
    not worth to debug/analyse further as it works with 16.20 fw

--- jni/offsets.c
+++ jni/offsets.c
@@ -12,11 +12,13 @@
 
 struct offsets offsets[] = {
 	// XZ1 Compact
-	{ "G8441_47.1.A.2.324",
-	  (void *)0xffffff800a8f84a0, (void *)0xffffff800a8f82b0, (void *)0xffffff800a8f4df4, (void *)0xffffff800a614490, (void *)0xffffff800a61de90 },
-	// XZ1 Compact
 	{ "G8441_47.1.A.8.49",
 	  (void *)0xffffff800a903460, (void *)0xffffff800a903270, (void *)0xffffff800a8ffdb4, (void *)0xffffff800a614490, (void *)0xffffff800a61deb0 },
+
+#ifdef INCLUDE_MOST_LIKELY_NOT_WORKING_TARGETS
+	// XZ1 Compact
+	{ "G8441_47.1.A.2.324",
+	  (void *)0xffffff800a8f84a0, (void *)0xffffff800a8f82b0, (void *)0xffffff800a8f4df4, (void *)0xffffff800a614490, (void *)0xffffff800a61de90 },
 	// XZ1
 	{ "G8341_47.1.A.2.324",
 	  (void *)0xffffff800a8f84a0, (void *)0xffffff800a8f82b0, (void *)0xffffff800a8f4df4, (void *)0xffffff800a614490, (void *)0xffffff800a61de90 },
@@ -29,6 +31,7 @@ struct offsets offsets[] = {
 	// XZ Premium dual
 	{ "G8142_47.1.A.3.254",
 	  (void *)0xffffff800a901460, (void *)0xffffff800a901270, (void *)0xffffff800a8fddb4, (void *)0xffffff800a614490, (void *)0xffffff800a61deb0 },
+#endif
 
 	// XZ1 Compact
 	{ "G8441_47.1.A.16.20",
